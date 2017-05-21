12091a9dcdf263443fe6b9da77435be484a8d224
Alex Bird <alexebird@gmail.com>
2017-05-21 01:10:09 -0700

    bump ver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Trellifier.Mixfile do
 
   def project do
     [app: :trellifier,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
